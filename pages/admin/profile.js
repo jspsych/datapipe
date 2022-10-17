dc4cb0d019a7f4c78047a449f92a65a43f5941ba
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-10-17 13:58:32 -0400

    add update token

--- pages/admin/profile.js
+++ pages/admin/profile.js
@@ -1,10 +1,26 @@
 import AuthCheck from "../../components/AuthCheck"
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "../../lib/firebase";
+import { useState } from "react";
 
 export default function ProfilePage({}) {
+
+  const [token, setToken] = useState('');
+
   return (
     <AuthCheck>
       <h1>Profile</h1>
-      <input type="text" id="osf-token" placeholder="OSF Token" />
+      <input type="text" id="osf-token" placeholder="OSF Token" value={token} onChange={(e)=>setToken(e.target.value)} />
+      <button onClick={() => handleSaveButton(token)}>Save</button>
     </AuthCheck>
   )
+}
+
+async function handleSaveButton(token) {
+  try {
+    const userDoc = doc(db, 'users', auth.currentUser.uid);
+    await setDoc(userDoc, {osfToken: token}, {merge: true});
+  } catch (error) {
+    console.log(error);
+  }
 }
