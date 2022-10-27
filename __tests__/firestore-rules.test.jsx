ac0dc8dc998f5a619e564fdc48fe5093356d3095
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-10-27 10:56:42 -0400

    add seedDB helper, test basic experiment owner rules

--- __tests__/firestore-rules.test.jsx
+++ __tests__/firestore-rules.test.jsx
@@ -11,9 +11,18 @@ import { doc, collection, getDoc, getDocs, setDoc, updateDoc, deleteDoc } from '
 import { readFileSync } from 'fs';
 let testEnv;
 
+async function seedDB(data){
+  await testEnv.withSecurityRulesDisabled(async (context) => {
+    const dbAdmin = context.firestore();
+    for (const [key, value] of Object.entries(data)) {
+      await setDoc(doc(dbAdmin, key), value);
+    }
+  });
+}
+
 beforeAll(async () => {
   testEnv = await initializeTestEnvironment({
-    projectId: 'test-project',
+    projectId: 'osf-relay',
     firestore: {
       rules:  readFileSync('firestore.rules', 'utf8'),
       host: 'localhost',
@@ -44,4 +53,39 @@ describe('/users', () => {
     
     await assertFails(getDoc(doc(user123.firestore(), 'users/user456')));
   });
+});
+
+describe('/experiments', () => {
+  it('should allow deny read access to unauthenticated users', async () => {
+    const unauth = testEnv.unauthenticatedContext();
+
+    await assertFails(getDoc(doc(unauth.firestore(), 'experiments/123')));
+  });
+
+  it('should allow read access to authenticated users for their own doc', async () => {
+    const data = {
+      'experiments/123': {
+        owner: 'user123',
+      }
+    }
+    await seedDB(data);
+
+    const user123 = testEnv.authenticatedContext('user123');
+
+    await assertSucceeds(getDoc(doc(user123.firestore(), 'experiments/123')));
+  });
+
+  it('should deny read access to authenticated users for their own doc', async () => {
+    const data = {
+      'experiments/456': {
+        owner: 'user456',
+      }
+    }
+    await seedDB(data);
+
+    const user123 = testEnv.authenticatedContext('user123');
+
+    await assertFails(getDoc(doc(user123.firestore(), 'experiments/456')));
+  });
+
 });
