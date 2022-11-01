0977d3613a8311da7775926d48426eb4e6a523e2
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-11-01 08:57:11 -0400

    ui for main page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,25 @@
 import Link from 'next/link';
 import { useContext } from 'react';
 import { UserContext } from '../lib/context';
-import { Box } from '@chakra-ui/react';
+import { Stack, Heading, Text, Button } from '@chakra-ui/react';
 
 export default function Home() {
 
   const { user } = useContext(UserContext);
 
   return (
-    <Box>
-      <h1>OSF Relay</h1>
-      <p>Connect your behavioral experiments to the OSF, for free.</p>
+    <Stack>
+      <Heading>OSF Relay</Heading>
+      <Text>Connect your behavioral experiments to the OSF, for free.</Text>
       {user ? 
         <Link href="/admin">Go to Dashboard</Link>
         :
-        <Link href="/signup">Create an Account</Link>
+        <Link href="/signup">
+          <Button variant={"solid"} colorScheme={"green"} size={"md"}>
+            Create an Account
+          </Button>
+        </Link>
       }
-    </Box>
+    </Stack>
   )
 }
