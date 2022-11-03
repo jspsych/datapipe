1a80cd764ce344b756d1705d7880b388028648a8
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-11-02 22:12:10 -0400

    add isSubmitting for signin

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -2,8 +2,10 @@ import { Button, Input, InputGroup, InputLeftAddon, Stack, Heading, FormControl,
 import { auth } from "../lib/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import Router from "next/router";
+import { useState } from "react";
 
 export default function SignInForm({routeAfterSignIn}) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
   return(
     <Stack>
       <Heading>Sign In</Heading>
@@ -15,19 +17,22 @@ export default function SignInForm({routeAfterSignIn}) {
         <FormLabel>Password</FormLabel>
         <Input type="password" />
       </FormControl>
-      <Button onClick={handleSignInButton}>Sign In</Button>
+      <Button onClick={()=>handleSignInButton(setIsSubmitting)} isLoading={isSubmitting}>Sign In</Button>
     </Stack>
   )
 }
 
-async function handleSignInButton() {
+async function handleSignInButton(setIsSubmitting) {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+
+  setIsSubmitting(true);
 
   try{
     const user = await signInWithEmailAndPassword(auth, email, password);
     Router.push(routeAfterSignIn);
   } catch(error) {
+    setIsSubmitting(false);
     console.log("Sign in failed");
     console.log(error);
   }
