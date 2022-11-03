7a5d89bfe82860f41dfa063fa3b9284364fadcab
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-11-03 09:35:22 -0400

    better features for experiment edit form

--- pages/admin/[experiment_id].js
+++ pages/admin/[experiment_id].js
@@ -1,13 +1,39 @@
-import AuthCheck from '../../components/AuthCheck';
-import { useRouter } from 'next/router';
-import { useDocumentData } from 'react-firebase-hooks/firestore';
-import { db, auth } from '../../lib/firebase';
+import AuthCheck from "../../components/AuthCheck";
+import { useRouter } from "next/router";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import { db, auth } from "../../lib/firebase";
 import { doc, setDoc } from "firebase/firestore";
 
-import { FormControl, Stack, Spinner, Input, InputGroup, InputLeftAddon, Checkbox, FormLabel, Button } from '@chakra-ui/react';
+import {
+  FormControl,
+  Stack,
+  Spinner,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Checkbox,
+  FormLabel,
+  Button,
+  ButtonGroup,
+  IconButton,
+  Editable,
+  EditableInput,
+  EditablePreview,
+  Flex,
+  Link,
+  Text,
+  Tag,
+  TagLabel,
+} from "@chakra-ui/react";
+import { useEditableControls } from "@chakra-ui/react";
+import {
+  EditIcon,
+  CheckIcon,
+  CloseIcon,
+  ExternalLinkIcon,
+} from "@chakra-ui/icons";
 
 export default function ExperimentPage() {
-
   const router = useRouter();
   const { experiment_id } = router.query;
 
@@ -15,52 +41,138 @@ export default function ExperimentPage() {
     <AuthCheck>
       <ExperimentEditForm expId={experiment_id} />
     </AuthCheck>
-  )
+  );
 }
 
-function ExperimentEditForm({expId}) {
-
-  const [data, loading, error, snapshot, reload] = useDocumentData(doc(db, `experiments/${expId}`));
+function ExperimentEditForm({ expId }) {
+  const [data, loading, error, snapshot, reload] = useDocumentData(
+    doc(db, `experiments/${expId}`)
+  );
   return (
     <>
       {loading && <Spinner color="green.500" size={"xl"} />}
-      {data &&
-      <Stack>
-        <FormControl id="title">
-          <FormLabel>Title</FormLabel>
-          <Input type="text" defaultValue={data.title} />
-        </FormControl>
-        <FormControl id="osf-repo">
-          <FormLabel>OSF Repo</FormLabel>
-          <Input type="text" defaultValue={data.osfRepo} />
-        </FormControl>
-        <FormControl id="active">
-          <FormLabel>Active</FormLabel>
-          <Checkbox defaultChecked={data.active} />
-        </FormControl>
-        <Button
-          variant={"solid"}
-          colorScheme={"green"}
-          size={"md"}
-          mr={4}
-          onClick={() => handleSaveButton(expId)}
-        >
-          Save
-        </Button>
-      </Stack>}
+      {data && (
+        <Stack>
+          <ExperimentTitle title={data.title} />
+          <Text>Parent OSF Project</Text>
+          <Link href={`https://osf.io/${data.osfRepo}`} isExternal>
+            {`https://osf.io/${data.osfRepo}`} <ExternalLinkIcon mx="2px" />
+          </Link>
+          <Text>OSF Data Component</Text>
+          <Link href={`https://osf.io/${data.osfComponent}`} isExternal>
+            {`https://osf.io/${data.osfComponent}`}{" "}
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+          <Text>Completed Sessions</Text>
+          <Text>{data.sessions}</Text>
+          <Text>
+            This experiment is currently{" "}
+            <Tag
+              size="sm"
+              variant="solid"
+              colorScheme={data.active ? "green" : "red"}
+            >
+              <TagLabel>{data.active ? "Active" : "Inactive"}</TagLabel>
+            </Tag>
+          </Text>
+          {data.active ? (
+            <Button
+              variant={"solid"}
+              colorScheme="red"
+              onClick={()=>deactivateExperiment(expId)}
+            >
+              Deactivate Experiment
+            </Button>
+          ) : (
+            <Button
+              variant={"solid"}
+              colorScheme="green"
+              onClick={()=>activateExperiment(expId)}
+            >
+              Activate Experiment
+            </Button>
+          )}
+        </Stack>
+      )}
     </>
-  )
+  );
+}
+
+function ExperimentTitle({ title }) {
+  /* Here's a custom control */
+  function EditableControls() {
+    const {
+      isEditing,
+      getSubmitButtonProps,
+      getCancelButtonProps,
+      getEditButtonProps,
+    } = useEditableControls();
+
+    return isEditing ? (
+      <>
+        <IconButton icon={<CheckIcon />} {...getSubmitButtonProps()} />
+        <IconButton icon={<CloseIcon />} {...getCancelButtonProps()} />
+      </>
+    ) : (
+      <IconButton size="sm" icon={<EditIcon />} {...getEditButtonProps()} />
+    );
+  }
+
+  return (
+    <Editable
+      textAlign="center"
+      defaultValue={title}
+      fontSize="2xl"
+      isPreviewFocusable={false}
+    >
+      <EditablePreview />
+      {/* Here is the custom input */}
+      <Input as={EditableInput} />
+      <EditableControls />
+    </Editable>
+  );
 }
 
 async function handleSaveButton(expId) {
   try {
-    await setDoc(doc(db, `experiments/${expId}`), {
-      title: document.querySelector('#title').value,
-      osfRepo: document.querySelector('#osf-repo').value,
-      active: document.querySelector('#active').checked,
-    }, {merge: true});
+    await setDoc(
+      doc(db, `experiments/${expId}`),
+      {
+        title: document.querySelector("#title").value,
+        osfRepo: document.querySelector("#osf-repo").value,
+        active: document.querySelector("#active").checked,
+      },
+      { merge: true }
+    );
+  } catch (error) {
+    console.error(error);
   }
-  catch (error) {
+}
+
+async function activateExperiment(expId){
+  try {
+    await setDoc(
+      doc(db, `experiments/${expId}`),
+      {
+        active: true,
+      },
+      { merge: true }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function deactivateExperiment(expId){
+  try {
+    await setDoc(
+      doc(db, `experiments/${expId}`),
+      {
+        active: false,
+      },
+      { merge: true }
+    );
+  } catch (error) {
     console.error(error);
   }
 }
