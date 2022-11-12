60089735b459cfd8b3ad9aace9455e543e676d6f
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-11-12 14:59:46 -0500

    trying size param

--- pages/help.js
+++ pages/help.js
@@ -3,7 +3,7 @@ import { Stack, Heading, Text, Button } from '@chakra-ui/react';
 export default function Help() {
     return (
         <Stack>
-        <Heading>How does this all work?</Heading>
+        <Heading as="h1" size="3xl">How does this all work?</Heading>
         <Text>Help content goes here.</Text>
         </Stack>
     )
