a8976868e07c755de87faccd7327ad594c46dee7
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-11-19 14:32:25 -0500

    add error handling to signinform

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -1,4 +1,4 @@
-import { Button, Input, Text, Link as ChakraLink, InputGroup, InputLeftAddon, Stack, Heading, FormControl, FormLabel, FormHelperText } from "@chakra-ui/react";
+import { Button, Input, Text, Link as ChakraLink, InputGroup, InputLeftAddon, Stack, Heading, FormControl, FormLabel, FormHelperText, FormErrorMessage } from "@chakra-ui/react";
 import { auth } from "../lib/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import Router from "next/router";
@@ -7,25 +7,29 @@ import Link from "next/link";
 
 export default function SignInForm({routeAfterSignIn}) {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [invalidEmail, setInvalidEmail] = useState(false);
+  const [invalidPassword, setInvalidPassword] = useState(false);
   return(
     <Stack>
       <Heading pb={2}>Sign In</Heading>
-      <FormControl id="email">
+      <FormControl id="email" isInvalid={invalidEmail}>
         <FormLabel>Email</FormLabel>
         <Input type="email" />
+        <FormErrorMessage>Invalid email</FormErrorMessage>
       </FormControl>
-      <FormControl id="password" pb={4}>
+      <FormControl id="password" pb={4} isInvalid={invalidPassword}>
         <FormLabel>Password</FormLabel>
-        <Input type="password" />
-        <FormHelperText><Link href="/reset-password" passHref><ChakraLink>Forgot password?</ChakraLink></Link></FormHelperText>
+        <Input type="password" onChange={()=>setInvalidPassword(false)}/>
+        <FormErrorMessage>Invalid password</FormErrorMessage>
       </FormControl>
-      <Button onClick={()=>handleSignInButton(setIsSubmitting, routeAfterSignIn)} isLoading={isSubmitting}>Sign In</Button>
+      <Text><Link href="/reset-password" passHref><ChakraLink>Forgot password?</ChakraLink></Link></Text>
+      <Button onClick={()=>handleSignInButton(setIsSubmitting, setInvalidEmail, setInvalidPassword, routeAfterSignIn)} isLoading={isSubmitting}>Sign In</Button>
       <Text pt={4}>Need an account? <Link href="/signup" passHref><ChakraLink>Sign up</ChakraLink></Link></Text>
     </Stack>
   )
 }
 
-async function handleSignInButton(setIsSubmitting, routeAfterSignIn) {
+async function handleSignInButton(setIsSubmitting, setInvalidEmail, setInvalidPassword, routeAfterSignIn) {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
@@ -36,6 +40,12 @@ async function handleSignInButton(setIsSubmitting, routeAfterSignIn) {
     Router.push(routeAfterSignIn);
   } catch(error) {
     setIsSubmitting(false);
+    if(error.code == "auth/user-not-found") {
+      setInvalidEmail(true);
+    }
+    if(error.code == "auth/wrong-password") {
+      setInvalidPassword(true);
+    }
     console.log("Sign in failed");
     console.log(error);
   }
