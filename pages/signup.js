a5ac152200cc586441ffa927fe35aa5cd8d9fa5b
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-11-19 14:22:23 -0500

    add some basic error handling to signup page

--- pages/signup.js
+++ pages/signup.js
@@ -4,27 +4,35 @@ import { doc, setDoc } from "firebase/firestore";
 import { auth, db } from "../lib/firebase";
 
 import Router from "next/router";
+import { useState } from "react";
 
-import { FormControl, Stack, Spinner, Input, InputGroup, InputLeftAddon, Checkbox, FormLabel, Button } from '@chakra-ui/react';
+import { FormControl, Stack, Spinner, Input, InputGroup, InputLeftAddon, Checkbox, FormLabel, Button, FormErrorMessage } from '@chakra-ui/react';
 
 export default function SignUpPage({}) {
 
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [invalidEmail, setInvalidEmail] = useState(false);
+  const [weakPassword, setWeakPassword] = useState(false);
+
   return (
     <Stack>
-      <FormControl id="email">
+      <FormControl id="email" isInvalid={invalidEmail}>
         <FormLabel>Email</FormLabel>
-        <Input type="email" />
+        <Input type="email" onChange={()=>setInvalidEmail(false)}/>
+        <FormErrorMessage>Invalid email</FormErrorMessage>
       </FormControl>
-      <FormControl id="password" pb={4}>
+      <FormControl id="password" pb={4} isInvalid={weakPassword}>
         <FormLabel>Password</FormLabel>
-        <Input type="password" />
+        <Input type="password" onChange={()=>setWeakPassword(false)}/>
+        <FormErrorMessage>Password must be at least 6 characters</FormErrorMessage>
       </FormControl>
       <Button
         variant={"solid"}
         colorScheme={"green"}
         size={"md"}
         mr={4}
-        onClick={handleCreateAccount}
+        onClick={()=>handleCreateAccount(setIsSubmitting, setInvalidEmail, setWeakPassword)}
+        isLoading={isSubmitting}
       >
         Create Account
       </Button>
@@ -32,9 +40,11 @@ export default function SignUpPage({}) {
   );
 }
 
-async function handleCreateAccount() {
+async function handleCreateAccount(setIsSubmitting, setInvalidEmail, setWeakPassword) {
   const email = document.querySelector('#email').value;
   const password = document.querySelector('#password').value;
+
+  setIsSubmitting(true);
   
   try {
     const userCredential = await createUserWithEmailAndPassword(
@@ -54,6 +64,16 @@ async function handleCreateAccount() {
     Router.push("/admin")
   }
   catch (error) {
+    if(error.code == "auth/email-already-in-use") {
+      // do something.
+    }
+    if(error.code == "auth/weak-password") {
+      setWeakPassword(true);
+    }
+    if(error.code == "auth/invalid-email") {
+      setInvalidEmail(true);
+    }
+    setIsSubmitting(false);
     console.log(error);
   }
 }
