6e82dae43268279a1029024f5c9b1d40178397d5
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-11-23 11:05:49 -0500

    remove next.js api route

--- pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
-
-// see this for CORS stuff: https://github.com/vercel/next.js/blob/canary/examples/api-routes-cors/pages/api/cors.ts
