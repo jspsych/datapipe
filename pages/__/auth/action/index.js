e3dee6923d6fb7c360890036aeabc8683498e1bf
Jan Horubała <jan.horubala@gmail.com>
2022-11-26 20:21:07 +0100

    Add page to handle firebase LINK url

---
+++ pages/__/auth/action/index.js
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react';
+
+export default function Action() {
+  const router = useRouter();
+
+  useEffect(() => {
+    switch (router?.query?.mode) {
+      case "resetPassword": router.push('/reset-password?token=' + router?.query?.oobCode);
+      default: router.push('/')
+    }  
+  }, [])
+  
+  return <></>;
+}
