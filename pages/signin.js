216db6a1f5db9f510c25e0a12128e8ab16944e04
Jan Horubała <jan.horubala@gmail.com>
2022-11-26 19:09:10 +0100

    Wrap sign in form with card

--- pages/signin.js
+++ pages/signin.js
@@ -1,11 +1,5 @@
 import SignInForm from "../components/SignInForm";
-import { Box } from "@chakra-ui/react";
 
-export default function SignInPage({}) {
-
-  return (
-    <Box>
-      <SignInForm routeAfterSignIn={"/admin"}/>
-    </Box>
-  )
+export default function SignInPage() {
+  return <SignInForm routeAfterSignIn={"/admin"}/>;
 }
