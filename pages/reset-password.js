e6664c3fa9303e5a2c631b287275444e156d5db0
Jan Horubała <jan.horubala@gmail.com>
2022-11-26 20:21:32 +0100

    WIP reset password with token

--- pages/reset-password.js
+++ pages/reset-password.js
@@ -11,23 +11,43 @@ import {
 import { auth } from "../lib/firebase";
 import { sendPasswordResetEmail } from "firebase/auth";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from 'next/router'
 
 export default function ResetPassword() {
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [sentEmail, setSentEmail] = useState(false);
+  const router = useRouter()
+  const [state, setState] = useState('forgot');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  useEffect(() => {
+    if (router?.query?.token) {
+      setState('token');
+    }
+  }, [])
+
   return (
     <Stack>
-      {sentEmail && (
+      {state === 'send' && (
         <Text>
           An email has been sent with a link to reset your password.
         </Text>
       )}
-      {!sentEmail && (
+      {state === 'forgot' && (
         <>
           <Heading as="h2" size="xl">
             Forgot your password?
           </Heading>
+          <Text>We will send you an email with a link to reset.</Text>
+          <FormControl id="reset-email">
+            <FormLabel>Email</FormLabel>
+            <Input type="email" />
+          </FormControl>
+          <Button isLoading={isSubmitting} onClick={() => handleResetPasswordButton(setIsSubmitting, setSentEmail)}>
+            Request Reset
+          </Button>
+        </>
+      )}
+      {state === 'token' && (
+        <>
           <Text>We will send you an email with a link to reset.</Text>
           <FormControl id="reset-email">
             <FormLabel>Email</FormLabel>
@@ -50,7 +70,7 @@ async function handleResetPasswordButton(setIsSubmitting, setSentEmail) {
   try {
     await sendPasswordResetEmail(auth, email);
     setIsSubmitting(false);
-    setSentEmail(true);
+    setState('send');
   } catch (error) {
     setIsSubmitting(false);
     console.log("Password reset failed");
