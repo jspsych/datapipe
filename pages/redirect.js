7b9ae8fa3b1d50fa6ee46ec05dbe0757a298d940
Jan Horubała <jan.horubala@gmail.com>
2022-11-27 16:43:57 +0100

    Move firebase link to just /redirect

--- pages/redirect.js
+++ pages/redirect.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react';
 
-export default function Action() {
+export default function Redirect() {
   const router = useRouter();
 
   useEffect(() => {
