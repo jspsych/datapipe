5e024156e2e98fa021e50d4512eeeeb944c9d711
Jan Horubała <jan.horubala@gmail.com>
2022-11-29 12:35:10 +0100

    Refactor sign in

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -1,14 +1,50 @@
-import { Button, Card, CardBody, CardHeader, Input, Text, Link as ChakraLink, Stack, Heading, FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Input,
+  Text,
+  Link as ChakraLink,
+  Stack,
+  Heading,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from "@chakra-ui/react";
 import { auth } from "../lib/firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import Router from "next/router";
 import { useState } from "react";
+import { useRouter } from 'next/router';
 import Link from "next/link";
+import { ERROR, getError } from "../lib/utils";
 
-export default function SignInForm({routeAfterSignIn}) {
+export default function SignInForm({ routeAfterSignIn }) {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [invalidEmail, setInvalidEmail] = useState(false);
-  const [invalidPassword, setInvalidPassword] = useState(false);
+
+  const onSubmit = async () => {
+    setIsSubmitting(true);
+    try {
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      router.push(routeAfterSignIn);
+    } catch(error) {
+      setIsSubmitting(false);
+      const { code } = error;
+      if (code == ERROR.PASSWORD_WRONG) {
+        setErrorPassword(getError(code));
+      } else {
+        setErrorEmail(getError(code));
+      }
+      console.log("Sign in failed");
+      console.log(error);
+    }  
+  }
+
   return(
     <Card w={360}>
       <CardHeader>
@@ -16,15 +52,15 @@ export default function SignInForm({routeAfterSignIn}) {
       </CardHeader>
       <CardBody>
         <Stack>
-          <FormControl id="email" isInvalid={invalidEmail}>
+          <FormControl isInvalid={errorEmail}>
             <FormLabel>Email</FormLabel>
-            <Input type="email" />
-            <FormErrorMessage>Invalid email</FormErrorMessage>
+            <Input type="email" onChange={e => { setEmail(e.target.value); setErrorEmail('') }} />
+            <FormErrorMessage>{errorEmail}</FormErrorMessage>
           </FormControl>
-          <FormControl id="password" pb={4} isInvalid={invalidPassword}>
+          <FormControl pb={4} isInvalid={errorPassword}>
             <FormLabel>Password</FormLabel>
-            <Input type="password" onChange={()=>setInvalidPassword(false)}/>
-            <FormErrorMessage>Invalid password</FormErrorMessage>
+            <Input type="password" onChange={e => { setPassword(e.target.value); setErrorPassword('') }} />
+            <FormErrorMessage>{errorPassword}</FormErrorMessage>
           </FormControl>
           <Text>
             <Link href="/reset-password" passHref><ChakraLink>Forgot password?</ChakraLink></Link>
@@ -32,7 +68,7 @@ export default function SignInForm({routeAfterSignIn}) {
           <Button
             colorScheme={"green"}
             isLoading={isSubmitting}
-            onClick={()=>handleSignInButton(setIsSubmitting, setInvalidEmail, setInvalidPassword, routeAfterSignIn)}>
+            onClick={onSubmit}>
             Sign In
           </Button>
           <Text pt={4}>Need an account? <Link href="/signup" passHref><ChakraLink>Sign Up!</ChakraLink></Link></Text>
@@ -40,27 +76,4 @@ export default function SignInForm({routeAfterSignIn}) {
       </CardBody>
     </Card>
   )
-}
-
-async function handleSignInButton(setIsSubmitting, setInvalidEmail, setInvalidPassword, routeAfterSignIn) {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  setIsSubmitting(true);
-
-  try{
-    const user = await signInWithEmailAndPassword(auth, email, password);
-    Router.push(routeAfterSignIn);
-  } catch(error) {
-    setIsSubmitting(false);
-    if(error.code == "auth/user-not-found") {
-      setInvalidEmail(true);
-    }
-    if(error.code == "auth/wrong-password") {
-      setInvalidPassword(true);
-    }
-    console.log("Sign in failed");
-    console.log(error);
-  }
-    
 }
