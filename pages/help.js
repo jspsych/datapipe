520315597d8d1b181c77481951c4c8dbd5619dfe
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-12-13 15:58:57 -0500

    add help

--- pages/help.js
+++ pages/help.js
@@ -1,10 +1,14 @@
-import { Stack, Heading, Text, Button } from '@chakra-ui/react';
+import { Stack, Heading, Text, Button } from "@chakra-ui/react";
 
 export default function Help() {
-    return (
-        <Stack>
-        <Heading as="h1" size="3xl">How does this all work?</Heading>
-        <Text>Help content goes here.</Text>
-        </Stack>
-    )
+  return (
+    <Stack w={640}>
+      <Heading as="h1" size="2xl">
+        Getting Started
+      </Heading>
+      <Text>DataPipe is a free service that lets you connect your behavioral experiments with the Open Science Framework so that any data you collect is automatically saved in an OSF component.</Text>
+      <Text>We are working on building out the instructional materials. For now, please watch this tutorial video to learn how to use DataPipe.</Text>
+      <Text>We are especially appreciative of feedback during this early stage of the project. Please let us know what needs improvement as we work towards a stable release.</Text>
+    </Stack>
+  );
 }
