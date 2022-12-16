700a0361c73d1c4c08ecfe22dd3e2ada9d5e6c8e
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-12-16 11:04:43 -0500

    catch errors during CSV validation

--- functions/validate-csv.js
+++ functions/validate-csv.js
@@ -1,15 +1,19 @@
 import * as CSV from "csv-string";
 
 export default function validateCSV(csv, requiredFields) {
-  let parsedCSV = null;
   try {
-    parsedCSV = CSV.parse(csv);
+    let parsedCSV = null;
+    try {
+      parsedCSV = CSV.parse(csv);
+    } catch (error) {
+      return false;
+    }
+    if (requiredFields.every((field) => parsedCSV[0].includes(field))) {
+      return true;
+    } else {
+      return false;
+    }
   } catch (error) {
-    return false;
-  }
-  if (requiredFields.every((field) => parsedCSV[0].includes(field))) {
-    return true;
-  } else {
     return false;
   }
 }
