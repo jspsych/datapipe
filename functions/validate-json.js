b8193ae7380b82f88c75bf27fb2c1e9513bc6479
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-12-16 11:02:58 -0500

    cleanup code

--- functions/validate-json.js
+++ functions/validate-json.js
@@ -1,7 +1,4 @@
-
 export default function validateJSON(json, requiredFields) {
-  
-
   let parsedJSON = null;
   try {
     parsedJSON = JSON.parse(json);
@@ -9,7 +6,7 @@ export default function validateJSON(json, requiredFields) {
     return false;
   }
 
-  if(Array.isArray(parsedJSON)){
+  if (Array.isArray(parsedJSON)) {
     return parsedJSON.some((trial) => {
       const keys = Object.keys(trial);
       return requiredFields.every((field) => keys.includes(field));
@@ -18,26 +15,4 @@ export default function validateJSON(json, requiredFields) {
     const keys = Object.keys(parsedJSON);
     return requiredFields.every((field) => keys.includes(field));
   }
-
-  const { error } = schema.validate(parsedJSON);
-  if (error) {
-    console.log(error);
-    return false;
-  }
-  return true;
-}
-
-function createSchema(requiredFields) {
-
-  const fieldMap = new Map(requiredFields.map((field) => [field, Joi.any().required()]));
-  console.log(fieldMap);
-
-  const trialSchema = Joi.object(fieldMap);
-
-  const arraySchema = Joi.array().has(trialSchema);
-  const objectSchema = trialSchema;
-
-  const schema = Joi.alternatives().try(arraySchema, objectSchema);
-
-  return schema;
 }
