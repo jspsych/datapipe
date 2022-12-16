e3d8d6324cef9fe17c0429fa07ba44e9243c1512
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-12-16 11:04:04 -0500

    catch any error during validation and return false

--- functions/validate-json.js
+++ functions/validate-json.js
@@ -1,18 +1,22 @@
 export default function validateJSON(json, requiredFields) {
-  let parsedJSON = null;
   try {
-    parsedJSON = JSON.parse(json);
+    let parsedJSON = null;
+    try {
+      parsedJSON = JSON.parse(json);
+    } catch (error) {
+      return false;
+    }
+
+    if (Array.isArray(parsedJSON)) {
+      return parsedJSON.some((trial) => {
+        const keys = Object.keys(trial);
+        return requiredFields.every((field) => keys.includes(field));
+      });
+    } else {
+      const keys = Object.keys(parsedJSON);
+      return requiredFields.every((field) => keys.includes(field));
+    }
   } catch (error) {
     return false;
-  }
-
-  if (Array.isArray(parsedJSON)) {
-    return parsedJSON.some((trial) => {
-      const keys = Object.keys(trial);
-      return requiredFields.every((field) => keys.includes(field));
-    });
-  } else {
-    const keys = Object.keys(parsedJSON);
-    return requiredFields.every((field) => keys.includes(field));
   }
 }
