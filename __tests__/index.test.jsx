08edd1e3459bbf5dc53dbe6a8f215598c6063edf
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-12-28 14:29:33 -0500

    fix failing test

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import Home from '../pages/index';
-import '@testing-library/jest-dom';
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+import "@testing-library/jest-dom";
 
-describe('Home', () => {
-  it('renders Home component', () => {
+describe("Home", () => {
+  it("renders Home component", () => {
     render(<Home />);
-    expect(screen.getByText('OSF Relay')).toBeInTheDocument();
+    expect(screen.getByText("Create an account")).toBeInTheDocument();
   });
 });
