42ac95e8ff58b85c24ec28f7267f580ee9f022b7
Josh de Leeuw <josh.deleeuw@gmail.com>
2022-12-28 14:29:48 -0500

    remove old commented imports

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,3 @@
-// import functions from "firebase-functions";
-// import { initializeApp } from "firebase-admin/app";
-// import { FieldValue, getFirestore } from "firebase-admin/firestore";
-// import express from "express";
-// import fetch from "node-fetch";
-// import cors from "cors";
-// import validateJSON from "./validate-json.js";
-// import validateCSV from "./validate-csv.js";
-// import putFileOSF from "./put-file-osf.js";
 import { apiData } from "./api-data.js";
 import { apiCondition } from "./api-condition.js";
 import { apiBase64 } from "./api-base64.js";
