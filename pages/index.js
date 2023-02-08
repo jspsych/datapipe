da5338899b3454ff8adee39fe230869f92fa0fe7
jadeddelta <101148768+jadeddelta@users.noreply.github.com>
2023-02-08 00:19:02 -0500

    fix homepipe resize bug, flexbasis to Feature and free feature display

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,20 @@
 import Link from "next/link";
 import { useContext } from "react";
 import { UserContext } from "../lib/context";
+import { Stack, VStack, Text, Button, Image } from "@chakra-ui/react";
 import {
-  Stack,
-  VStack,
-  Container,
-  Heading,
-  Text,
-  Button,
-  Image,
-  HStack,
-} from "@chakra-ui/react";
-import { AddIcon, EditIcon, RepeatClockIcon } from "@chakra-ui/icons";
+  AddIcon,
+  EditIcon,
+  RepeatClockIcon,
+  RepeatIcon,
+} from "@chakra-ui/icons";
 
 export default function Home() {
   const { user } = useContext(UserContext);
 
-  const Feature = ({ title, text, icon }) => {
+  const Feature = ({ title, text, icon, flexBasis }) => {
     return (
-      <VStack flexBasis={"33%"}>
+      <VStack flexBasis={flexBasis} textAlign={"center"}>
         {icon}
         <Text fontSize={["xl", "2xl"]} fontWeight="semibold">
           {title}
@@ -29,7 +25,7 @@ export default function Home() {
   };
 
   return (
-    <VStack maxW={["95%", "1400px"]} px={[2, 12]} spacing={["1rem", "2rem"]}>
+    <VStack maxW={["95%", "1400px"]} px={[2, 12]} spacing={["1.5rem", "3rem"]}>
       <Stack
         direction={["column", "row"]}
         flexDirection={["column-reverse", "row"]}
@@ -67,6 +63,7 @@ export default function Home() {
           alt="Decorative illustration of a pipe with data flowing through it"
           boxSize={["100%", "768px"]}
           quality={100}
+          objectFit={"contain"}
         />
       </Stack>
       <Stack
@@ -74,13 +71,15 @@ export default function Home() {
         w={"100%"}
         flexDirection={["column-reverse", "row"]}
         align="center"
+        spacing={["1rem", "4rem"]}
       >
         <Feature
           icon={<AddIcon w={8} h={8} color={"brandOrange.500"} />}
           title={"Link your OSF Account"}
           text={
-            "Simply create an OSF project or use an existing one, along with generating an OSF token to send files directly to it."
+            "Simply create an OSF project or use an existing one, along with generating an OSF token to allow DataPipe to send data to it."
           }
+          flexBasis={"33%"}
         />
         <Feature
           icon={<EditIcon w={8} h={8} color={"brandOrange.500"} />}
@@ -88,6 +87,7 @@ export default function Home() {
           text={
             "Configure your experiment parameters and add our pre-generated code snippets into your web-based experiment."
           }
+          flexBasis={"33%"}
         />
         <Feature
           icon={<RepeatClockIcon w={8} h={8} color={"brandOrange.500"} />}
@@ -95,6 +95,32 @@ export default function Home() {
           text={
             "Host your experiment on a static hosting website like Github Pages or Netlify, activate data collection, and sit back as data will be automatically sent to OSF."
           }
+          flexBasis={"33%"}
+        />
+      </Stack>
+      <Stack
+        direction={["column", "row"]}
+        w={"100%"}
+        flexDirection={["column-reverse", "row"]}
+        align="center"
+        spacing={["1rem", "2rem"]}
+      >
+        <Feature
+          icon={<RepeatIcon w={8} h={8} color={"brandOrange.500"} />}
+          title={"100% Free"}
+          text={
+            "With the Open Science Foundation as a methods to store data for free, " +
+            "placed upon traditionally free web page hosting, means less worrying about " +
+            "development and pricing, and more time spent on your experiments."
+          }
+          flexBasis={"50%"}
+        />
+        <Image
+          src={"/xxxx.png"} //placeholder
+          alt={"Picture of code with arrows pointing to many screens"}
+          boxSize={["100%", "384px"]}
+          quality={100}
+          objectFit={"contain"}
         />
       </Stack>
     </VStack>
