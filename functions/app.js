bd3354de0cea5f64a16392ea0da2282c1fd0097c
Robert Hawkins <hawkrobe@gmail.com>
2025-08-21 22:11:38 -0700

    revert app.js

--- functions/app.js
+++ functions/app.js
@@ -2,9 +2,7 @@ import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 // Use the same project ID as the emulator
-const app = initializeApp({
-  projectId: "demo-project"
-});
+const app = initializeApp();
 const db = getFirestore(app);
 
 export { db };
